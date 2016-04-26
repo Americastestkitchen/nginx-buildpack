0a27c3b70d2c1273e7489a60af7424e4ad93dcd6
ebressler <eric.bressler@americastestkitchen.com>
2016-04-26 14:55:30 +0100

    updating for further nginx support

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -63,6 +63,7 @@ echo "Building Nginx ${NGINX_VERSION}"
 	cd nginx-${NGINX_VERSION}
 	./configure \
 		--with-pcre=pcre-${PCRE_VERSION} \
+		--with-http_ssl_module \
 		--with-stream_ssl_module \
 		--prefix=/tmp/nginx \
 		--add-module=/${temp_dir}/nginx-${NGINX_VERSION}/headers-more-nginx-module-${HEADERS_MORE_VERSION} \
